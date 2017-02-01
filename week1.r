c6a71dc031eb152a840380dbcbbbd4fa05c96f4b
pablovicente <pablovicente93@usal.es>
2017-01-31 19:19:31 -0500

    Week 1 completed

--- week1.r
+++ week1.r
@@ -1,5 +1,5 @@
 # Set working directory
-
+setwd('/Users/pablo/Desktop/Pablo/MS_in_Computer_Science/Data\ Visualization/data-visualization')
 
 ############################
 ### 	Just once		 ###
@@ -61,6 +61,8 @@ install.packages("ggtheme")
 install.packages("dplyr")
 install.packages("reshape")
 install.packages("data.table")
+install.packages('rmarkdown')
+install.packages('plotly')
 
 library(ggplot2)
 library(ggrepel)
@@ -69,6 +71,8 @@ library(ggtheme)
 library(dplyr)
 library(reshape)
 library(data.table)
+library(rmarkdown)
+library(plotly)
 
 
 # Loading housing data
@@ -122,7 +126,7 @@ p1 +
 
 
 ############################
-### 	  GAPMINDER		 ###
+### 	   GAPMINDER	  	 ###
 ############################  
 
 install.packages("gapminder")
@@ -155,3 +159,153 @@ p + aes(color=continent) + geom_point() + geom_smooth(lwd=2, se=FALSE)
 
 p + geom_point(aes(color=continent, alpha=pop), size=2) + facet_grid(. ~continent)
 p + geom_point(aes(color=continent, alpha=pop), size=2) + facet_grid(. ~continent) + geom_smooth(color='black', lwd=1, se=FALSE)
+
+############################
+###      BABYNAMES       ###
+############################
+
+# Loading required libraries
+library(babynames)
+library(ggplot2)    # the king of plotting 
+library(magrittr)   # chain operators, e.g. to "pipe" a value forward
+library(dplyr)      # for data manipulation 
+
+
+
+# Summary of information
+str(babynames)
+summary(babynames)
+
+# Displaying first rows
+head(babynames)
+
+
+# Counting different names
+length(unique(babynames$name))
+
+# How many kids in database
+sum(babynames$n)/10^6
+
+# Plotting for both sexs
+ggplot(babynames, aes(year, n)) +
+  geom_line(data = filter(babynames, name=="James"))
+
+# We can see that with the following command
+head(filter(babynames, name=="James"))
+
+# Filtering by name but also splitting by sex
+ggplot(babynames, aes(year, n)) +
+  geom_line(data = filter(babynames, name=="James"), aes(color=sex))
+
+
+# Try to follow this code chunk at home, using dplyr() and magrittr()
+top10 <- babynames %>%
+  group_by(sex, name) %>%
+  summarize(total = sum(n)) %>%
+  arrange(desc(total)) %>%
+  group_by(sex) %>%
+  mutate(rank=row_number()) %>%
+  filter(rank<=10)  %>%
+  arrange(sex, rank)
+
+top10f <- top10 %>% filter(sex=="F")
+top10m <- top10 %>% filter(sex=="M")  
+
+# Plotting most 10 common names 
+babynames %>%
+  filter(sex=="F") %>%
+  filter(name %in% top10f$name) %>%
+  ggplot(., aes(year, n)) +
+  geom_line(aes(color=name, group=name))
+
+babynames %>%
+  filter(sex=="M") %>%
+  filter(name %in% top10m$name) %>%
+  ggplot(., aes(year, n)) +
+  geom_line(aes(color=name, group=name))
+
+
+#Plot the most common names in 2013 over the entire period.
+top10_2013 <- babynames %>%
+  filter(year==2013) %>%
+  group_by(sex, name) %>%  
+  summarize(total = sum(n)) %>%
+  arrange(desc(total)) %>%
+  group_by(sex) %>%
+  mutate(rank=row_number()) %>%
+  filter(rank<=10)  %>%
+  arrange(sex, rank)
+
+top10f_2013 <- top10_2013 %>% filter(sex=="F")
+top10m_2013 <- top10_2013 %>% filter(sex=="M") 
+top10u_2013 <- Map(c, top10_2013 %>% filter(sex=="M") %>% head(., n=5), top10_2013 %>% filter(sex=="F") %>% head(., n=5))
+
+
+babynames %>%
+  filter(sex=="F") %>%
+  filter(name %in% top10f_2013$name) %>%
+  ggplot(., aes(year, n)) +
+  geom_line(aes(color=name, group=name))
+
+babynames %>%
+  filter(sex=="M") %>%
+  filter(name %in% top10m_2013$name) %>%
+  ggplot(., aes(year, n)) +
+  geom_line(aes(color=name, group=name))
+
+
+#Explore which names are most often used as unisex names. 
+top10_unisex <- babynames %>%
+  group_by(sex,name) %>%
+  summarize(total = sum(n)) %>%
+  group_by(name) %>%
+  filter(n() > 1) %>%
+  group_by(sex)
+
+f <- top10_unisex %>% filter(sex=="F") %>% arrange(desc(name))
+m <- top10_unisex %>% filter(sex=="M") %>% arrange(desc(name))
+m['diff'] <- abs(m$total - f$total)
+names_diff <- m[order(-m$diff), ]
+
+head(names_diff)
+
+tail(names_diff)
+
+#For which names has the popularity over time changed a lot?  
+CV <- function(mean, sd){
+      (sd/mean)*100
+      }
+
+baby_stats <- babynames %>% 
+  group_by(sex) %>% 
+  as.data.frame() %>% 
+  aggregate(n~name, ., mean)
+
+baby_stats <- plyr::rename(baby_stats, c("n"="mean"))
+
+baby_stats['sd'] <- babynames %>% 
+  group_by(sex) %>% 
+  as.data.frame() %>% 
+  aggregate(n~name, ., sd) %>%
+  .$n
+
+baby_stats['cv'] <- CV(baby_stats$mean, baby_stats$sd)
+baby_stats <- baby_stats %>% arrange(desc(cv))
+
+top10_var <- head(baby_stats, n = 10)$name
+
+top10_records <- babynames %>%
+  filter(name %in% top10_var) %>% 
+  group_by(name,year) %>%
+  summarize(total = sum(n))
+
+
+ggplot(data = top10_records, aes(year, total)) +
+  geom_line( aes(color=name, group=name))
+
+
+
+
+
+
+
